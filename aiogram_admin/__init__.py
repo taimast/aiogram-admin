989c95b117075077cb7a1b104fc07c6e7005477f
taimast <81189712+taimast@users.noreply.github.com>
2022-11-09 18:13:08 +0300

    minor fix

--- aiogram_admin/__init__.py
+++ aiogram_admin/__init__.py
@@ -5,6 +5,7 @@ from aiogram import Dispatcher
 from aiogram_admin import models, config
 from aiogram_admin.filters.channel_subscription_filter import ChannelSubscriptionFilter
 from aiogram_admin.handlers.admin import register_admin_handlers
+from aiogram_admin.handlers.common import register_common_handlers
 from aiogram_admin.handlers.errors.errors_handlers import register_error
 from aiogram_admin.models import BaseSubsChat, BaseUser
 from aiogram_admin.utils import TempData
@@ -36,7 +37,7 @@ async def setup_admin_handlers(dp: Dispatcher,
     models.BaseUser = User
     config.ADMIN_COMMAND = admin_command or config.ADMIN_COMMAND
     register_admin_handlers(dp, admins)
-
+    register_common_handlers(dp)
     if temp_data is None:
         temp_data = TempData(subscription_channels=await SubsChat.all())
     dp.workflow_data.update(temp_data=temp_data)
